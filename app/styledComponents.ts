9d244e22a12796dd8a42b760e455e394cdbb22b9
Shivambhart1 <shivama.21.becs@acharya.ac.in>
2024-07-06 13:40:12 -0800

    pushing files

---
+++ app/styledComponents.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 4rem;
+`;
+
+export const HeroSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["assets.aceternity.com"],
+  },
+};
+export default nextConfig;
